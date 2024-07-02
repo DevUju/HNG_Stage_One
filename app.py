0954ca1b0a8b45a8004e9fa454c37faafa057ccc
Obianuju1998 <agnesanosike@gmail.com>
2024-07-02 19:32:08 +0100

    Reformated my code

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
 from flask import Flask, request, jsonify
-from geopy.geocoders import Nominatim
 import requests
 
 app = Flask(__name__)
 
-IP = request.headers.getlist("X-Forwarded-For")
+
 API_KEY = "6ed1d1b943ab46b9804145041240207"
-URL = f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={IP}"
 
 def get_user_location():
     try:
-        response = requests.get(url=URL)
-        data = response.json()
-        lat, lon = data['loc'].split(',')
-        geolocator = Nominatim(user_agent="location_search")
-        location = geolocator.reverse((lat, lon))
-        return location
+        response = requests.get('https://api.ipgeolocation.io/getip')
+        return response.json()
     except:
         print("Error: Unable to detect your location.")
         return None
 
 @app.route('/api/hello', methods=['GET'])
 def hello():
-    visitor_name = input("Enter your name: ")
-    # Get location from IP address
-    location = get_user_location()
-    # Get temperature from weather API
-    response= requests.get(url=URL)
-    weather_data = response.json()
-    temperature = weather_data['main']['temp']
+    visitor_name = request.args.get('name')
+    client_ip = get_user_location()['ip']
 
+    # Get temperature from weather API
+    response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={client_ip}")
+    weather_data = response.json()
+    temperature = weather_data['current']['temp_f']
+    location = weather_data['location']['region']
     greeting = f'Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {location}'
-    return jsonify({'client_ip': IP, 'location': location, 'greeting': greeting})
+
+    return jsonify({'client_ip': client_ip, 'location': location, 'greeting': greeting})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
-
